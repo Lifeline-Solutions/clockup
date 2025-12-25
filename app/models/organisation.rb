5748c43fe27c7a2b897e65452eb74ddcbc3f0ad3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-25 12:28:46 +0300

    Add validations for the working hours and added helper methods for lateness and early checkout

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -17,6 +17,17 @@ class Organisation < ApplicationRecord
   validates :allowed_radius_meters, presence: true,
                                     numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 1000, message: 'must be a positive number less than or equal to 1000 meters' }
   validates :clock_qr_token, presence: true, uniqueness: true
+  validates :work_start_time, :work_end_time, presence: true
+
+  # Returns a DateTime object for the organisation's start time on a given date
+  def work_start_time_for(date)
+    DateTime.new(date.year, date.month, date.day, work_start_time.hour, work_start_time.min, work_start_time.sec, work_start_time.zone)
+  end
+
+  # Returns a DateTime object for the organisation's end time on a given date
+  def work_end_time_for(date)
+    DateTime.new(date.year, date.month, date.day, work_end_time.hour, work_end_time.min, work_end_time.sec, work_end_time.zone)
+  end
 
   private
 
