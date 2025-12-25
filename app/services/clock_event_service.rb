bfe14d97ad036421fcd48eae031ebbab3568c028
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-25 13:31:16 +0300

    Ensure that the service handles both API and monolith requests for clock in and out

--- app/services/clock_event_service.rb
+++ app/services/clock_event_service.rb
@@ -6,9 +6,16 @@ class ClockEventService
   # organisation: Organisation instance
   # latitude, longitude: current user coordinates
   # event_type: optional, :clock_in or :clock_out
+  #
+  # Behaviour:
+  # - If event_type is provided, it is respected (monolith buttons)
+  # - If event_type is nil, it is inferred from the user's last event (mobile QR flow)
   def self.call(user:, organisation:, latitude:, longitude:, event_type: nil)
     # Determine last event in this organisation
-    last_event = ClockEvent.where(user: user, organisation: organisation).order(occurred_at: :desc).first
+    last_event = ClockEvent
+      .where(user: user, organisation: organisation)
+      .order(occurred_at: :desc)
+      .first
 
     # Decide event type if not passed
     event_type ||= if last_event.nil? || last_event.clock_out?
@@ -21,7 +28,12 @@ class ClockEventService
     raise DuplicateEventError, "Cannot create duplicate consecutive #{event_type} event" if last_event&.event_type == event_type.to_s
 
     # Calculate distance
-    distance = DistanceCalculator.distance(latitude, longitude, organisation.latitude, organisation.longitude)
+    distance = DistanceCalculator.distance(
+      latitude,
+      longitude,
+      organisation.latitude,
+      organisation.longitude
+    )
 
     # Geofence check
     if distance > organisation.allowed_radius_meters
@@ -40,9 +52,20 @@ class ClockEventService
       distance_from_org_meters: distance
     )
 
-    # Add lateness/early_leave info for reporting
-    clock_event.define_singleton_method(:late?) { clock_event.clock_in? && clock_event.occurred_at > organisation.work_start_time_for(clock_event.occurred_at.to_date) }
-    clock_event.define_singleton_method(:early_leave?) { clock_event.clock_out? && clock_event.occurred_at < organisation.work_end_time_for(clock_event.occurred_at.to_date) }
+    # Add lateness / early leave info for reporting (non-persisted helpers)
+    clock_event.define_singleton_method(:late?) do
+      clock_event.clock_in? &&
+        organisation.respond_to?(:work_start_time_for) &&
+        clock_event.occurred_at >
+          organisation.work_start_time_for(clock_event.occurred_at.to_date)
+    end
+
+    clock_event.define_singleton_method(:early_leave?) do
+      clock_event.clock_out? &&
+        organisation.respond_to?(:work_end_time_for) &&
+        clock_event.occurred_at <
+          organisation.work_end_time_for(clock_event.occurred_at.to_date)
+    end
 
     clock_event
   end
