cd24e97eebaf036443cad474cb53c596756a14b3
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-25 14:34:32 +0300

    Update included:   - Made it easy to keep track and list all the organisations with their details   - Rename the clock_events_controller to maintain Rails conventions

--- app/controllers/organisation_controller.rb
+++ app/controllers/organisation_controller.rb
@@ -1,2 +1,37 @@
 class OrganisationController < ApplicationController
+  def index
+    @organisations = Organisation.includes(:users)
+
+    # Build presence summaries per organisation
+    @presence_summary = {}
+    @organisations.each do |org|
+      user_ids = org.users.select(:id)
+      latest = ClockEvent
+        .where(organisation_id: org.id, user_id: user_ids)
+        .select('DISTINCT ON (user_id) clock_events.*')
+        .order('user_id, occurred_at DESC')
+      present_count = latest.count { |evt| evt.clock_in? }
+      @presence_summary[org.id] = { users_count: org.users.size, present_count: present_count }
+    end
+  end
+
+  def show
+    @organisation = Organisation.includes(:users).find(params[:id])
+
+    user_ids = @organisation.users.select(:id)
+
+    # Latest event per user for this organisation using Postgres DISTINCT ON
+    @latest_events = ClockEvent
+      .where(organisation_id: @organisation.id, user_id: user_ids)
+      .select('DISTINCT ON (user_id) clock_events.*')
+      .order('user_id, occurred_at DESC')
+      .includes(:user)
+      .index_by(&:user_id)
+
+    today_range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    @todays_events = ClockEvent
+      .where(organisation_id: @organisation.id, occurred_at: today_range)
+      .includes(:user)
+      .order(:occurred_at)
+  end
 end
