692c36e8f7b2c751c9c42b7d1971e99ba53ec0be
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-25 14:44:07 +0300

    Add the views for the functionalities worked on including those of the user, organisation and the late counts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = User.includes(:organisation).find(params[:id])
+    @organisation = @user.organisation
+
+    @latest_event = @user.latest_clock_event_for(@organisation)
+
+    today_range = Time.zone.now.beginning_of_day..Time.zone.now.end_of_day
+    @todays_events = @user.clock_events
+      .where(organisation_id: @organisation.id, occurred_at: today_range)
+      .order(:occurred_at)
+
+    @recent_events = @user.clock_events
+      .where(organisation_id: @organisation.id)
+      .order(occurred_at: :desc)
+      .limit(50)
+
+    @lateness_count = @recent_events.count { |e| e.late? }
+    @early_leave_count = @recent_events.count { |e| e.early_leave? }
+  end
+end
