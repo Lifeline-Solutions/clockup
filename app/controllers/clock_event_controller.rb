d5e6b342188742f29b4524f8846b362f3273cefe
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-25 12:30:07 +0300

    Update to my clockevent controller and service to ensure that it matches the requirement where clockin/out must be explicitely passed

--- app/controllers/clock_event_controller.rb
+++ app/controllers/clock_event_controller.rb
@@ -4,17 +4,24 @@ class ClockEventsController < ApplicationController
     user = current_user
     lat = params[:latitude]
     lon = params[:longitude]
+    type = params[:event_type]&.to_sym # :clock_in or :clock_out
+
+    unless %i[clock_in clock_out].include?(type)
+      flash[:alert] = 'Invalid event type'
+      return redirect_to organisation_path(org)
+    end
 
     clock_event = ClockEventService.call(
       user: user,
       organisation: org,
       latitude: lat,
-      longitude: lon
+      longitude: lon,
+      event_type: type
     )
 
     flash[:notice] = "#{clock_event.event_type.humanize} successful"
     redirect_to organisation_path(org)
-  rescue ClockEventService::OutsideGeofenceError => e
+  rescue ClockEventService::OutsideGeofenceError, ClockEventService::DuplicateEventError => e
     flash[:alert] = e.message
     redirect_to organisation_path(org)
   rescue ActiveRecord::RecordInvalid => e
--- app/services/clock_event_service.rb
+++ app/services/clock_event_service.rb
@@ -2,28 +2,26 @@ class ClockEventService
   class OutsideGeofenceError < StandardError; end
   class DuplicateEventError < StandardError; end
 
-  def self.call(user:, organisation:, latitude:, longitude:)
-    # Get last event for this user in this organisation
-    last_event = ClockEvent.where(user: user, organisation: organisation).order(occurred_at: :desc).first
-
-    # Decide event type automatically
-    event_type = if last_event.nil? || last_event.clock_out?
-                   :clock_in
-                 else
-                   :clock_out
-                 end
-
+  # user: User instance
+  # organisation: Organisation instance
+  # latitude, longitude: current user coordinates
+  # event_type: :clock_in or :clock_out
+  def self.call(user:, organisation:, latitude:, longitude:, event_type:)
     # Prevent duplicate consecutive events
+    last_event = ClockEvent.where(user: user, organisation: organisation).order(occurred_at: :desc).first
     raise DuplicateEventError, "Cannot create duplicate consecutive #{event_type} event" if last_event&.event_type == event_type.to_s
 
-    # Calculate distance from organisation
+    # Calculate distance
     distance = DistanceCalculator.distance(latitude, longitude, organisation.latitude, organisation.longitude)
 
-    # Check geofence
-    raise OutsideGeofenceError, "Cannot clock #{event_type}: user is #{distance}m away, allowed radius is #{organisation.allowed_radius_meters}m" if distance > organisation.allowed_radius_meters
+    # Geofence check
+    if distance > organisation.allowed_radius_meters
+      raise OutsideGeofenceError,
+            "Cannot clock #{event_type}: user is #{distance}m away, allowed radius is #{organisation.allowed_radius_meters}m"
+    end
 
-    # Create the ClockEvent
-    ClockEvent.create!(
+    # Create ClockEvent
+    clock_event = ClockEvent.create!(
       user: user,
       organisation: organisation,
       event_type: event_type,
@@ -32,5 +30,11 @@ class ClockEventService
       longitude: longitude,
       distance_from_org_meters: distance
     )
+
+    # Add lateness/early_leave info for reporting
+    clock_event.define_singleton_method(:late?) { clock_event.clock_in? && clock_event.occurred_at > organisation.work_start_time_for(clock_event.occurred_at.to_date) }
+    clock_event.define_singleton_method(:early_leave?) { clock_event.clock_out? && clock_event.occurred_at < organisation.work_end_time_for(clock_event.occurred_at.to_date) }
+
+    clock_event
   end
 end
