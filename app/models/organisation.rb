3df749eff33b811c98b4c34391c7bd1140ae1f91
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-25 10:01:53 +0300

    Ensure that before an organisation is created/ saved, we generate a secure token for it. This can be changed later in the admins dashboard

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,2 +1,10 @@
 class Organisation < ApplicationRecord
+  before_create :generate_clock_qr_token
+
+  private
+
+  # we can later rotate this token if needed via admin console
+  def generate_clock_qr_token
+    self.clock_qr_token ||= SecureRandom.hex(10) # 20-character token
+  end
 end
